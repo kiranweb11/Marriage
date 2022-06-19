83c84d2f1dbc389aaa50c42d8073977207026f48
kiranweb11 <kiran.ks1996@gmail.com>
2022-06-19 20:42:59 +0530

    commit to marriage

---
+++ java.js
@@ -0,0 +1,10 @@
+document.getElementById("btn").addEventListener("click",function(){
+    var man=document.getElementById("name").value;
+    var boss=document.getElementById("pass").value;
+    if(man=="kiran"&& boss=="kiran"){
+        document.getElementById("login").action="sub/index.html";
+    }
+    else{
+        alert("Enter Valid Username or Password")
+    }
+ });
